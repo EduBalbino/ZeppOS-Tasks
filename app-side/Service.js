d51231b80317863bc266243e8858b696fd9fa7f7
balbino <eduardobalbino1337@gmail.com>
2025-01-01 23:03:51 -0300

    changed submodule hopefully

--- app-side/Service.js
+++ app-side/Service.js
@@ -1,14 +1,12 @@
 import {handleFetchRequest} from "../lib/mmk/FetchForward";
 import {MessageBuilder} from "../lib/zeppos/message";
 import {LoginProvider} from "./login/LoginProvider";
-import {CalDAVProxy} from "./CalDAVProxy";
 
 const messageBuilder = new MessageBuilder();
 
 export class ZeppTasksSideService {
   init() {
     this.login = new LoginProvider();
-    this.caldavProxy = new CalDAVProxy();
 
     settings.settingsStorage.addListener("change", async (e) => {
       await this.handleSettingsChange(e);
@@ -32,22 +30,14 @@ export class ZeppTasksSideService {
         if(e.newValue === "\"login_started\"")
           await this.login.settingsBeginLogin();
         break;
-      case "nextcloud_url_validate":
-        await this.caldavProxy.validateNextcloudURL(JSON.parse(e.newValue));
-        break;
-      case "caldav_validate":
-        await this.caldavProxy.validateConfig(JSON.parse(e.newValue));
-        break;
       case "auth_token":
         await this.login.settingsProcessAuthToken(e.newValue);
-        this.caldavProxy.onConfigAvailable();
         break;
     }
   }
 
   async handleRequest(ctx, request) {
     handleFetchRequest(ctx, request);
-    await this.caldavProxy.handleRequest(ctx, request);
 
     if(request.package !== "tasks_login") return;
     switch(request.action) {
